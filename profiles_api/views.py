626812a217ef56201c5d5a19a8e35473beb8dfb6
Roshni_Surpur <rsurpur20@choate.edu>
2022-06-10 18:02:11 -0400

    added a new viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from profiles_api import serializers
+
+
 
 class HelloApiView(APIView):
     """Test API view"""
@@ -43,3 +46,46 @@ class HelloApiView(APIView):
     def delete(self,request,pk= None):
         """delete an object in database"""
         return Response({'method':'DELETE'})   
+
+class HelloViewSet(viewsets.ViewSet):
+    """tests api viewset"""
+
+    serializer_class  = serializers.HelloSerializer
+    def list(self, request):
+        """return a hello message"""
+        a_viewset = [
+            'uses actions (list,create, retrieve, update, partial_update)',
+            'automatically maps to urls using routers',
+            'provides more functionality with less code',
+        ]
+        return Response({ 'message': 'Hello',
+                        'a_viewset': a_viewset}) #converts list/dictionary to json to response
+
+    def create(self,request):
+        """create a new hello message"""
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello sup{name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self,request, pk = None): #pk is primary key
+        """handle getting an object by its id"""
+        return Response({'http_method': 'GET'})
+
+    def update(self,request, pk = None): #pk is primary key
+        """handle UPDATING an object """
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self,request, pk = None): #pk is primary key
+        """handle partially updating an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self,request, pk = None): #pk is primary key
+        """handle destroying an object """
+        return Response({'http_method': 'DELETE'})      
+        
