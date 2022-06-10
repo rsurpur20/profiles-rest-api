fb1e0fe0acb4a8c80524ef64f4f813c14e51c65d
Roshni_Surpur <rsurpur20@choate.edu>
2022-06-10 16:35:06 -0400

    added an http get function

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API view"""
+    def get(self,request,format=None):
+        """returns a list of api view features"""
+        an_apiview = [
+            'Uses HTTP Methods as functions (get, post, patch, put delete)',
+            'is similar to a traditional django view',
+            'gives you the most control ovr your application logic',
+            'is mapped manually to urls',
+        ]
+        #must return a reponse object
+        return Response({ 'message': 'Hello',
+                        'an_apiview': an_apiview}) #converts list/dictionary to json to response
+
+
