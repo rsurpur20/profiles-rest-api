d6178d68458ae7494563107ade4bf3b643119c79
Roshni_Surpur <rsurpur20@choate.edu>
2022-06-10 20:17:42 -0400

    tested authentication and login

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,6 +8,7 @@ router.register('hello-viewset', views.HelloViewSet, base_name = 'hello-viewset'
 router.register('profile',views.UserProfileViewSet) #no base name becasue we already hav a query set
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()), #register api view
+    path('hello-view/', views.HelloApiView.as_view()), #register api view,
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)) 
 ]
